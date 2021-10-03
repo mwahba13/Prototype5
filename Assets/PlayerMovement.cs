a5255c80d7eed36cb795d9fa0d6bd70dc8b525fb
Michael Wahba <mrswahba13@gmail.com>
2021-10-03 16:14:36 -0700

    player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public bool isPlayerMoving;
+    public float moveSpeed;
+
+    public GameObject[] lightsToTurnOff;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Gamepad gamepad = Gamepad.current;
+
+        float stickX = gamepad.leftStick.ReadValue().x;
+        float stickY = gamepad.leftStick.ReadValue().y;
+
+        if (stickX != 0 || stickY != 0)
+        {
+            isPlayerMoving = true;
+            foreach (var light in lightsToTurnOff)
+            {
+                light.SetActive(false);
+            }
+
+            transform.position += new Vector3(stickX, stickY, 0f) * moveSpeed * Time.deltaTime;
+
+            float heading = Mathf.Atan2(stickX, -stickY);
+            transform.rotation=Quaternion.Euler(0f,0f,heading*Mathf.Rad2Deg));
+
+        }
+        else
+        {
+            isPlayerMoving = false;
+            foreach (var light in lightsToTurnOff)
+            {
+                light.SetActive(true);
+            }
+        }
+    }
+}
