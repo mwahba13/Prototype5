75cd7ff9edccc97f0c22873c57971d7184743277
moechen97 <69623061+moechen97@users.noreply.github.com>
2021-10-04 00:19:10 -0700

    Add 2D NAVMESH and End State
    ahhhhhhh level design is so hard

---
+++ Assets/Yimeng_Test/KillerNavMesh.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class KillerNavMesh : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    [SerializeField] private GameObject deadPanel;
+
+    private PlayerMovement _player;
+    private NavMeshAgent agent;
+    private bool stop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        _player = FindObjectOfType<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_player.isPlayerMoving)
+        {
+            agent.isStopped = true;
+        }
+        else
+        {
+            agent.isStopped = false;
+        }
+        agent.SetDestination(target.transform.position);
+
+        float dist = Vector3.Distance(transform.position,target.transform.position);
+        
+        if (dist<=4.9f)
+        {
+            deadPanel.SetActive(true);
+        }
+        Debug.Log(dist);
+    }
+}
